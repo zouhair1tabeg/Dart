ced80889595cc5e4fe8bc6e0ad1aba0317f12428
Tabeg Zouhair <tabeg,zouhair@gmail.com>
2024-12-16 11:49:36 +0100

    "committed"

--- bin/boucle_for.dart
+++ bin/boucle_for.dart
@@ -1,14 +1,9 @@
-import 'dart:io';
-
 void main(List<String> arguments){
-  print("Compter (y/n)");
-  String validate;
-  validate = stdin.readLineSync()!.toLowerCase();
 
-  if(validate == "y"){
+  
     for(var compteur = 0 ; compteur <= 7 ; compteur++){
     print("Compteur: $compteur");
   }
-  }
+
   
 }
